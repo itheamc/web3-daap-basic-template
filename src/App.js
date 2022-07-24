c0be6a746ac4276a4c70badac3efac135dbc1372
Amit Chaudhary <itheamc@gmail.com>
2022-07-24 11:40:28 +0545

    code improvement

--- src/App.js
+++ src/App.js
@@ -1,22 +1,58 @@
-import logo from './logo.svg';
+import { useCallback } from 'react';
 import './App.css';
+import useWeb3 from './contracts/provider/useWeb3';
 
 function App() {
+
+  const { state: { web3, connected, selectedAccount, accounts }, dispatch } = useWeb3();
+
+  // callback hook to request the user to connect to their wallet
+  const tryConnect = useCallback(
+    async () => {
+      try {
+        const accounts_lst = await web3.eth.requestAccounts();
+        dispatch({
+          type: 'UPDATE_STATE',
+          payload: {
+            accounts: accounts_lst,
+            selectedAccount: accounts_lst[0],
+            connected: true,
+          }
+        });
+      } catch (error) {
+        dispatch({
+          type: 'UPDATE_STATE',
+          payload: {
+            error,
+          }
+        });
+      }
+    }, [dispatch, web3]);
+
+  // Function to initiate the callback hook
+  const requestWalletConnect = async () => {
+    await tryConnect();
+  }
+
+  // Function to disconnect the user from their wallet
+  const disconnect = async () => {
+    dispatch({
+      type: 'UPDATE_STATE',
+      payload: {
+        connected: false,
+        selectedAccount: null,
+        accounts: null,
+      }
+    });
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>{connected ? 'Connected' : 'Disconnected'}</h1>
+        {selectedAccount && <h2>{selectedAccount.substring(0, 7) + '...' + selectedAccount.substring(selectedAccount.length - 7)}</h2>}
+        {(connected && accounts) && accounts.length > 0 && <ol>{accounts.map((a) => <li key={a}>{a}</li>)}</ol>}
+        <button onClick={connected ? disconnect : requestWalletConnect}>{connected ? 'Disconnect' : 'Connect'}</button>
       </header>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -3,11 +3,15 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Web3Provider from './contracts/provider/Web3Provider';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Web3Provider>
+      <App />
+    </Web3Provider>
   </React.StrictMode>
 );
 
